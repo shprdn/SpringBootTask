a9263d7c4dd1565d6c2342eadbfccc2f23bf1d03
Shivani <shprdn@gmail.com>
2019-07-25 10:24:34 +0530

    committed

--- src/main/java/com/stackroute/muzix/MuzixAppApplication.java
+++ src/main/java/com/stackroute/muzix/MuzixAppApplication.java
@@ -1,8 +1,10 @@
 package com.stackroute.muzix;
 
 import com.stackroute.muzix.domain.Track;
+import com.stackroute.muzix.service.TrackService;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +17,18 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 //@ComponentScan
 public class MuzixAppApplication implements CommandLineRunner {
 
+	TrackService trackService;
+	@Autowired
+	public void setTrackService(TrackService trackService) {
+		this.trackService = trackService;
+	}
+
 	@Override
 	public void run(String... args) throws Exception {
+		//seed data to pre-fill the database with track
+		//information whenever the application starts
+		trackService.saveTrack(new Track(20,"main hoon hero tera","Armaan Malik"));
+		trackService.saveTrack(new Track(21,"suraj dooba hai","Amaal Malik"));
 		logger.info("Applicaton started using command line runner");
 	}
 
