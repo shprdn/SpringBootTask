7f717b88d72e09c73d7789222b0f1fbbfad27f73
shprdn <51949916+shprdn@users.noreply.github.com>
2019-08-08 09:33:49 +0530

    committed

---
+++ src/main/java/com/stackroute/muzix/exceptions/GlobalException.java
@@ -0,0 +1,20 @@
+package com.stackroute.muzixapp.exceptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+@ControllerAdvice
+public class GlobalException {
+   //This method handles the custom exception class TrackAlreadyExistsException
+   @ExceptionHandler(TrackAlreadyExistsException.class)
+   public ResponseEntity<String> playerExistsHandler(TrackAlreadyExistsException e){
+       return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
+   }
+   //This method handles the custom exception class TrackNotFoundException
+   @ExceptionHandler(TrackNotFoundException.class)
+   public ResponseEntity<String> playerNotFoundHandler(TrackNotFoundException e){
+       return new ResponseEntity<>(e.getMessage(),HttpStatus.CONFLICT);
+   }
+}
